f99e7cdd4fc4ba0dee7de09e1e66db824b098a8e
Hanefi Önaldı <Hanefi.Onaldi@Microsoft.com>
2020-12-09 13:32:27 +0300

    Add some more comments in tests

--- src/test/regress/sql/multi_partitioning.sql
+++ src/test/regress/sql/multi_partitioning.sql
@@ -1178,16 +1178,22 @@ PARTITION BY RANGE (work_ymdt);
 SELECT create_distributed_table('part_table', 'seq');
 
 --3. add a partition
-CREATE TABLE part_table_p202008 PARTITION OF part_table  FOR VALUES FROM ('2020-08-01 00:00:00') TO ('2020-09-01 00:00:00');
+CREATE TABLE part_table_p202008 PARTITION OF part_table FOR VALUES FROM ('2020-08-01 00:00:00') TO ('2020-09-01 00:00:00');
 
 --4. add a check constraint
 alter table part_table add CONSTRAINT my_seq CHECK (my_seq > 0);
 
 --5. add a partition
-CREATE TABLE part_table_p202009 PARTITION OF part_table  FOR VALUES FROM ('2020-09-01 00:00:00') TO ('2020-10-01 00:00:00');
+-- This test fails due to missing constraing "my_seq_1660199" where 1660199 is the shard id
+CREATE TABLE part_table_p202009 PARTITION OF part_table FOR VALUES FROM ('2020-09-01 00:00:00') TO ('2020-10-01 00:00:00');
 
+-- check the constraint names on the coordinator node
 \d+ partitioning_schema.part_table*
 
+-- check the constraint names on the worker node
+-- See that we append shardid to constraints created by an
+-- ALTER TABLE .. ADD CONSTRAINT, whereas we leave the constraint name as is if
+-- the constraint existed before we distributed the table.
 \c - - - :worker_1_port
 \d+ partitioning_schema.part_table*
 
