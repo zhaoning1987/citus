5b4e9fd22644e83279a2a94740a047a554966c8d
Marco Slot <marco.slot@gmail.com>
2020-12-16 20:55:40 +0100

    Add citus_set_coordinator_host

--- src/backend/distributed/sql/citus--9.5-1--10.0-1.sql
+++ src/backend/distributed/sql/citus--9.5-1--10.0-1.sql
@@ -2,5 +2,6 @@
 
 #include "udfs/citus_tables/10.0-1.sql"
 #include "udfs/citus_finish_pg_upgrade/10.0-1.sql"
+#include "udfs/citus_set_coordinator_host/10.0-1.sql"
 
 #include "../../columnar/sql/columnar--9.5-1--10.0-1.sql"
